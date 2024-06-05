623f2d2620e5591533c2ef91427d23ab28a3916d
yagmurtasanyurek <yagmurtasanyurek@gmail.com>
2024-06-05 22:59:27 +0300

    frac

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,9 @@ import View from './View.js';
 
 //import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
+//import { Fraction } from 'fractional';
+//import fracty from "fracty"
+import numberToFraction from './helpers.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
