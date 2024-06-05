43ebf7b9ab988ed6e17675b73e59e4c72300c6e5
yagmurtasanyurek <yagmurtasanyurek@gmail.com>
2024-06-05 23:08:05 +0300

    Changed fraction

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -33,38 +33,6 @@ export const AJAX = async function (url, uploadData = undefined) {
   }
 };
 
-/* export const getJSON = async function (url) {
-  try {
-    const fetchPro = fetch(url);
-    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    return data;
-  } catch (err) {
-    throw err;
-  }
-};
-
-export const sendJSON = async function (url, uploadData) {
-  try {
-    const fetchPro = fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(uploadData),
-    });
-    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    return data;
-  } catch (err) {
-    throw err;
-  }
-}; */
-
 export const numberToFraction = function (amount) {
   // This is a whole number and doesn't need modification.
   if (parseFloat(amount) === parseInt(amount)) {
@@ -96,3 +64,35 @@ export const numberToFraction = function (amount) {
   }
   return amount;
 };
+
+/* export const getJSON = async function (url) {
+  try {
+    const fetchPro = fetch(url);
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const sendJSON = async function (url, uploadData) {
+  try {
+    const fetchPro = fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(uploadData),
+    });
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}; */
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,9 +2,9 @@ import View from './View.js';
 
 //import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
+import numberToFraction from './helpers.js';
 //import { Fraction } from 'fractional';
 //import fracty from "fracty"
-import numberToFraction from './helpers.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
