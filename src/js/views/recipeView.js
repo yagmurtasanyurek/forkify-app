a9c196694e6c239dc619345b977df1e4cd3854a3
yagmurtasanyurek <yagmurtasanyurek@gmail.com>
2024-06-05 23:18:22 +0300

    fracty added

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,9 @@ import View from './View.js';
 
 //import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import numberToFraction from './helpers.js';
+
+import fracty from 'fracty';
+//import { numberToFraction } from '../helpers.js';
 //import { Fraction } from 'fractional';
 //import fracty from "fracty"
 
@@ -129,7 +131,7 @@ class RecipeView extends View {
     <use href="src/img/icons.svg#icon-check"></use>
   </svg>
   <div class="recipe__quantity"> ✔  ${
-    ing.quantity ? new Fraction(ing.quantity).toString() : ''
+    ing.quantity ? fracty(ing.quantity).toString() : ''
   }</div>
   <div class="recipe__description">
     <span class="recipe__unit">${ing.unit}</span>
