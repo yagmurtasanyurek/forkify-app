48f27b9f0b37db1d6860588578ee2df35ab13948
yagmurtasanyurek <yagmurtasanyurek@gmail.com>
2024-06-05 22:12:11 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -128,6 +128,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('welcome to the application!');
+};
+
 //Publisher - Subscriber Pattern
 const init = function () {
   bookmarksView.addHandlerRender(conrolBookmarks);
@@ -137,6 +141,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
