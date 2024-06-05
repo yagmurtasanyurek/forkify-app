a6b9a1bc0df3870c9a80f23147623645b43acb63
yagmurtasanyurek <yagmurtasanyurek@gmail.com>
2024-06-05 23:52:24 +0300

    Cleaned up controller2

--- src/js/controller.js
+++ src/js/controller.js
@@ -131,6 +131,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
